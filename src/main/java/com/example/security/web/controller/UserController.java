02db4de3472441eab6337192ca38731476bf1f97
Luke_YS <yjsmk0902@gmail.com>
2023-02-08 15:06:04 +0900

    Update UserController.java

--- src/main/java/com/example/security/web/controller/UserController.java
+++ src/main/java/com/example/security/web/controller/UserController.java
@@ -3,8 +3,14 @@ package com.example.security.web.controller;
 import com.example.security.domain.dto.UserDto;
 import com.example.security.web.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,6 +20,12 @@ public class UserController {
     @PostMapping("/user")
     public String signup(UserDto userDto) {
         userService.save(userDto);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
