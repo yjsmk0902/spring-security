8d12a9f35c72ddf8c5283b95772fdff05d6b500f
Luke_YS <yjsmk0902@gmail.com>
2023-03-07 11:17:49 +0900

    초기설정

---
+++ src/test/java/study/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
