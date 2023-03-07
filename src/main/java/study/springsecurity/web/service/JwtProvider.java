139e5ad841ec72efaee7ad8ea6ffc44952ac7c05
Luke_YS <yjsmk0902@gmail.com>
2023-03-07 16:28:23 +0900

    JwtProvider 수정

--- src/main/java/study/springsecurity/web/service/JwtProvider.java
+++ src/main/java/study/springsecurity/web/service/JwtProvider.java
@@ -1,11 +1,24 @@
 package study.springsecurity.web.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import study.springsecurity.domain.entity.Authority;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -21,6 +34,54 @@ public class JwtProvider {
 
     @PostConstruct
     protected void init() {
-        secretKey = Keys
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
     }
+
+    //토큰 생성
+    public String createToken(String account, List<Authority> roles) {
+        Claims claims = Jwts.claims().setSubject(account);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiredTime))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //권한정보 획득
+    //Spring Security 인증과정에서 권한확인을 위한 기능
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailService.loadUserByUsername(this.getAccount(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    //토큰에 담겨있는 유저 account 획득
+    private String getAccount(String token) {
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    //Authentication Header를 통해 인증을 한다.
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    //토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            //Bearer 검증
+            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+                return false;
+            } else {
+                token = token.split(" ")[1].trim();
+            }
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            //만료 시에 false
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
