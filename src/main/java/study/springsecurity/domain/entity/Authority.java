67c63f922762f800e6c2ea123a81a50d30bfc8c0
Luke_YS <yjsmk0902@gmail.com>
2023-03-07 11:34:27 +0900

    Authority 엔티티 추가

---
+++ src/main/java/study/springsecurity/domain/entity/Authority.java
@@ -0,0 +1,32 @@
+package study.springsecurity.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    private String name;
+
+    @JoinColumn(name = "member")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
