0ca0f31acf06303878d7f126c4960f5c25357c2a
Luke_YS <yjsmk0902@gmail.com>
2023-03-08 09:21:40 +0900

    JwtAuthenticationFilter 추가

---
+++ src/main/java/study/springsecurity/web/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package study.springsecurity.web.security.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//JWT가 유효성을 검사하는 필터
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(request);
+
+        if (token != null && jwtProvider.validateToken(token)) {
+            //접근 가능한 토큰인지 확인한다.
+            token = token.split(" ")[1].trim();
+            Authentication auth = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
